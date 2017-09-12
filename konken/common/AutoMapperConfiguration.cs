b65b2413f0c928a4be5206f7a616a0a5892e9dc7
Espen Halvorsen <espenh@unipluss.no>
2017-09-12 17:53:31 +0200

    web (mvc) og common (modeller og automapping)

---
+++ konken/common/AutoMapperConfiguration.cs
@@ -0,0 +1,98 @@
+﻿using AutoMapper;
+
+namespace common
+{
+	public static class AutoMapperConfiguration
+	{
+		public static void ConfigureStatic()
+		{
+			Mapper.Initialize(cfg =>
+			{
+				cfg.AddProfile<LeagueProfile>();
+				cfg.AddProfile<PlayerProfile>();
+				cfg.AddProfile<GameweekProfile>();
+			});
+		}
+
+		public static void Configure()
+		{
+			var config = new MapperConfiguration(cfg =>
+			{
+				cfg.AddProfile<LeagueProfile>();
+				cfg.AddProfile<PlayerProfile>();
+				cfg.AddProfile<GameweekProfile>();
+			});
+			config.AssertConfigurationIsValid();
+		}
+	}
+
+	public class LeagueProfile : Profile
+	{
+		public LeagueProfile()
+		{
+			CreateMap<LeagueEntity, League>()
+				;
+			CreateMap<League, LeagueEntity>()
+				;
+			CreateMap<LeagueEntity, LeagueGameweek>()
+				;
+		}
+	}
+
+	public class PlayerProfile : Profile
+	{
+		public PlayerProfile()
+		{
+			CreateMap<PlayerEntity, Player>()
+				;
+			CreateMap<Player, PlayerEntity>()
+				;
+		}
+	}
+
+	public class GameweekProfile : Profile
+	{
+		public GameweekProfile()
+		{
+			CreateMap<GameweekEntity, Gameweek>()
+				.ForMember(dest => dest.Cup, opt => opt.ResolveUsing<GameweekCupResolver>())
+				;
+			CreateMap<Gameweek, GameweekEntity>()
+				.ForMember(dest => dest.HomeFplPlayerId, opt => opt.MapFrom(src => src.Cup.HomeFplPlayerId))
+				.ForMember(dest => dest.HomeName, opt => opt.MapFrom(src => src.Cup.HomeName))
+				.ForMember(dest => dest.HomeTeamName, opt => opt.MapFrom(src => src.Cup.HomeTeamName))
+				.ForMember(dest => dest.HomePoints, opt => opt.MapFrom(src => src.Cup.HomePoints))
+				.ForMember(dest => dest.AwayFplPlayerId, opt => opt.MapFrom(src => src.Cup.AwayFplPlayerId))
+				.ForMember(dest => dest.AwayName, opt => opt.MapFrom(src => src.Cup.AwayName))
+				.ForMember(dest => dest.AwayTeamName, opt => opt.MapFrom(src => src.Cup.AwayTeamName))
+				.ForMember(dest => dest.AwayPoints, opt => opt.MapFrom(src => src.Cup.AwayPoints))
+				;
+			CreateMap<GameweekEntity, PlayerGameweek>()
+				;
+		}
+	}
+
+	public class GameweekCupResolver : IValueResolver<GameweekEntity, Gameweek, Cup>
+	{
+		public Cup Resolve(GameweekEntity source, Gameweek destination, Cup destMember, ResolutionContext context)
+		{
+			if (source.HomeFplPlayerId == null || source.HomeName == null || source.HomeTeamName == null ||
+			    source.HomePoints == null || source.AwayFplPlayerId == null || source.AwayName == null ||
+			    source.AwayTeamName == null || source.AwayPoints == null)
+				return null;
+
+			return new Cup()
+			{
+				GamewekkNumber = source.Number,
+				HomeFplPlayerId = source.HomeFplPlayerId,
+				HomeName = source.HomeName,
+				HomeTeamName = source.HomeTeamName,
+				HomePoints = source.HomePoints,
+				AwayFplPlayerId = source.AwayFplPlayerId,
+				AwayName = source.AwayName,
+				AwayTeamName = source.AwayTeamName,
+				AwayPoints = source.AwayPoints
+			};
+		}
+	}
+}
---
+++ konken/web/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using AutoMapper;
+
+namespace web
+{
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+			var builder = new ContainerBuilder();
+
+			// Register your MVC controllers. (MvcApplication is the name of
+			// the class in Global.asax.)
+			builder.RegisterControllers(typeof(MvcApplication).Assembly).PropertiesAutowired();
+
+			//AutoMapper
+			var autoMapperProfileTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes().Where(p => typeof(Profile).IsAssignableFrom(p) && p.IsPublic && !p.IsAbstract));
+			var autoMapperProfiles = autoMapperProfileTypes.Select(p => (Profile)Activator.CreateInstance(p));
+			builder.Register(ctx => new MapperConfiguration(cfg =>
+			{
+				foreach (var profile in autoMapperProfiles)
+				{
+					cfg.AddProfile(profile);
+				}
+			}));
+			builder.Register(ctx => ctx.Resolve<MapperConfiguration>().CreateMapper()).As<IMapper>().PropertiesAutowired();
+
+			// Set the dependency resolver to be Autofac.
+			var container = builder.Build();
+			DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+		}
+	}
+}
