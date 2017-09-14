86ba3b7cc0af3165f70df032ec7070755957fec1
Espen Halvorsen <espenh@unipluss.no>
2017-09-14 09:58:48 +0200

    trying to fix automapper / autofac problem

--- konken/web/Global.asax.cs
+++ konken/web/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -45,30 +46,29 @@ namespace web
 			builder.RegisterControllers(typeof(MvcApplication).Assembly).PropertiesAutowired();
 
 			////AutoMapper
-			//var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-			//builder.RegisterAssemblyTypes(assemblies)
-			//	.Where(t => typeof(Profile).IsAssignableFrom(t) && !t.IsAbstract && t.IsPublic).As<Profile>();
+			builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AssignableTo(typeof(Profile)).As<Profile>();
 
-			//builder.Register(c => new MapperConfiguration(cfg =>
-			//{
-			//	foreach (var profile in c.Resolve<IEnumerable<Profile>>())
-			//	{
-			//		cfg.AddProfile(profile);
-			//	}
-			//})).AsSelf().SingleInstance();
-
-			//builder.Register(c => c.Resolve<MapperConfiguration>().CreateMapper(c.Resolve)).As<IMapper>()
-			//	.InstancePerLifetimeScope().PropertiesAutowired();
-			var autoMapperProfileTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes().Where(p => typeof(Profile).IsAssignableFrom(p) && p.IsPublic && !p.IsAbstract));
-			var autoMapperProfiles = autoMapperProfileTypes.Select(p => (Profile)Activator.CreateInstance(p));
-			builder.Register(ctx => new MapperConfiguration(cfg =>
+			builder.Register(c => new MapperConfiguration(cfg =>
 			{
-				foreach (var profile in autoMapperProfiles)
+				foreach (var profile in c.Resolve<IEnumerable<Profile>>())
 				{
 					cfg.AddProfile(profile);
 				}
 			})).AsSelf().SingleInstance();
-			builder.Register(ctx => ctx.Resolve<MapperConfiguration>().CreateMapper()).As<IMapper>().InstancePerLifetimeScope().PropertiesAutowired();
+
+			builder.Register(ctx => ctx.Resolve<MapperConfiguration>().CreateMapper()).As<IMapper>().PropertiesAutowired();
+			//var autoMapperProfileTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()
+			//	.Where(p => typeof(Profile).IsAssignableFrom(p) && p.IsPublic && !p.IsAbstract));
+			//var autoMapperProfiles = autoMapperProfileTypes.Select(p => (Profile) Activator.CreateInstance(p));
+			//builder.Register(ctx => new MapperConfiguration(cfg =>
+			//{
+			//	foreach (var profile in autoMapperProfiles)
+			//	{
+			//		cfg.AddProfile(profile);
+			//	}
+			//})).AsSelf().SingleInstance();
+			//builder.Register(ctx => ctx.Resolve<MapperConfiguration>().CreateMapper()).As<IMapper>().InstancePerLifetimeScope()
+			//	.PropertiesAutowired();
 
 			// Set the dependency resolver to be Autofac.
 			var container = builder.Build();
