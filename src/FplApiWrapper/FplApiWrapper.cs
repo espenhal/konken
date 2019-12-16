80d431f73fba9bb8e94f80a7abd229b3022832b1
espenhal <espenhal@users.noreply.github.com>
2019-12-16 17:44:15 +0100

    kick off "new" season 19/20! :soccer:

---
+++ src/FplApiWrapper/FplApiWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FplApiWrapper
+{
+    public class FplApiWrapper : IFplApiWrapper
+    {   
+    }
+    
+    public interface IFplApiWrapper
+    {
+    }
+}
---
+++ src/web/Controllers/LeagueController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeagueController : ControllerBase
+    {
+        [HttpGet("{leagueId:Guid}")]
+        public async Task<IActionResult> Get(Guid leagueId)
+        {
+            var league = "{\"yolo\": \"yeah!\"}";
+            
+            return Ok(league);
+        }
+    }
+}
