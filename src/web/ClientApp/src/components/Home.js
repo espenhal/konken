49fceff7a94d09d09a4040ae679a984606e9d504
espenhal <espenhal@users.noreply.github.com>
2020-01-27 23:26:00 +0100

    Update Home.js

--- src/web/ClientApp/src/components/Home.js
+++ src/web/ClientApp/src/components/Home.js
@@ -56,7 +56,7 @@ export class Home extends Component {
 
         return (
             <div>
-                <h1>Alle oppdrag</h1>
+                <h1>FPL konken 19/20</h1>
                 {contents}
             </div>
         );
